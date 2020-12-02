881f2035236fe7dd17e5ac2f6d17065929c30b74
JazC <j_chan77@yahoo.ca>
2020-12-01 16:23:31 -0800

    Main page added

--- js/index-js.js
+++ js/index-js.js
@@ -6,7 +6,27 @@ document.getElementById("profile-button").addEventListener("click", function(eve
   location.href = "profile.html";
 });
 
+// Clicking any of the flashcard buttons will take user to the flashcard page
+document.getElementById("first-button").addEventListener("click", function(event) {
+  location.href = "flashcard.html";
+});
 
-document.getElementById("flashcard-button").addEventListener("click", function(event) {
+document.getElementById("second-button").addEventListener("click", function(event) {
+  location.href = "flashcard.html";
+});
+
+document.getElementById("third-button").addEventListener("click", function(event) {
+  location.href = "flashcard.html";
+});
+
+document.getElementById("fourth-button").addEventListener("click", function(event) {
+  location.href = "flashcard.html";
+});
+
+document.getElementById("fifth-button").addEventListener("click", function(event) {
+  location.href = "flashcard.html";
+});
+
+document.getElementById("sixth-button").addEventListener("click", function(event) {
   location.href = "flashcard.html";
 });
